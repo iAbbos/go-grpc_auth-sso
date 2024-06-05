40d749394a1af07769c24d2350e7ac6ef2200f1d
iAbbos <50768679+iAbbos@users.noreply.github.com>
2024-06-05 11:58:20 +0500

    init

---
+++ cmd/ssh/main.go
@@ -0,0 +1,11 @@
+package ssh
+
+func main() {
+	//TODO: Implement config
+
+	//TODO: Implement logger
+
+	//TODO: Implement application
+
+	//TODO: Implement grpc server
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/iAbbos/go-grpc_auth-sso
+
+go 1.22.0
+
+require github.com/iAbbos/go-grpc_auth-protos v0.0.1 // indirect
