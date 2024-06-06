0b9d2f916c5ae2f79c3eb5eff4cfc503540b7bb1
iAbbos <50768679+iAbbos@users.noreply.github.com>
2024-06-06 12:13:17 +0500

    [00:52:32-01:03:56] - impl slog;

---
+++ Makefile
@@ -0,0 +1,2 @@
+start:
+	 CONFIG_PATH=config/local.yaml go run cmd/ssh/main.go
--- cmd/ssh/main.go
+++ cmd/ssh/main.go
@@ -1,19 +1,58 @@
 package main
 
 import (
-	"fmt"
 	"github.com/iAbbos/go-grpc_auth-sso/internal/config"
+	"github.com/iAbbos/go-grpc_auth-sso/internal/pkg/lib/logger/handlers/slogpretty"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	//Implementing config
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+
+	log.Info("Starting application", slog.Any("cfg", cfg))
 
 	//TODO: Implement logger
 
 	//TODO: Implement application
 
 	//TODO: Implement grpc server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{Level: slog.LevelDebug},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
